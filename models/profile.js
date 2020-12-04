27d0678610399c9219bce504ee963a07f433c0e0
yoonseokch <yoonsukch@gmail.com>
2020-12-04 21:35:10 +0900

    skills routes add

--- models/profile.js
+++ models/profile.js
@@ -23,5 +23,28 @@ module.exports = (sequelize, Datatypes) => {
         freezeTableName: true,
         timestamps: false,
       }),
+      'Skills': sequelize.define('Skills', {
+        ID:
+        {
+            type: Datatypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Name:
+        {
+            type: Datatypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        Description:
+        {
+            type: Datatypes.STRING,
+            allowNull: false
+        }
+      },
+      {
+        freezeTableName: true,
+        timestamps: false,
+      }),
      }
 };
--- routes/profile.js
+++ routes/profile.js
@@ -9,4 +9,12 @@ router.get('/personalprofile',(req,res)=>{
         res.status(404).json(err);
     })
 })
+router.get('/skills',(req,res)=>{
+    db.Profile.Skills.findAll()
+    .then((data)=>{
+        res.status(200).json(data);
+    }).catch((err)=>{
+        res.status(404).json(err);
+    })
+})
 module.exports = router;
